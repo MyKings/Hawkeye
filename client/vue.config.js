b831a611e658ebd85164c8411152c5f654abdbb2
MyKings <xsseroot@gmail.com>
2023-03-10 09:29:15 +0800

    fix: 调整目录结构

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,29 +1,5 @@
-module.exports = {
-  runtimeCompiler: true,
-
-  css: undefined,
-
-  chainWebpack: config => {
-    config.module
-      .rule("vue")
-      .use("vue-loader")
-      .loader("vue-loader")
-      .tap(options => {
-        return options;
-      });
-  },
-  devServer: {
-    proxy: {
-      "/api/": {
-        target: "http://0.0.0.0:8999",
-        changeOrigin: true
-      }
-    }
-  },
-  baseUrl: undefined,
-  outputDir: undefined,
-  assetsDir: undefined,
-  filenameHashing: true,
-  productionSourceMap: false,
-  parallel: undefined
-};
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  lintOnSave: false
+})
